80a96173fb7beb407c78fb7c6e12ec737c1c00ba
Dipti2015 <99551838+Dipti2015@users.noreply.github.com>
2022-02-12 22:27:33 +0530

    Add files via upload

---
+++ Week 1 Q1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+printf("Name:DIPTIMAYEE SAHOO\n");
+printf("Regd no.:2102050073\n");
+printf("Branch:Electrical Engineering\n");
+printf("Hobbies: Writing poetries, Painting");
+getchar();
+}
